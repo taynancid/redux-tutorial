8c219bd3c6d257dbf8ff8da2358ecdf9b03baa4c
Taynan Siqueira <taynacid123@gmail.com>
2019-01-03 09:59:17 -0200

    complete core of a store with a reducer example

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-function createStore() {
+//Library code
+function createStore(reducer) {
   // the store should have four parts
   // 1. the state
   // 2. get the state
@@ -6,10 +7,34 @@ function createStore() {
   // 4. update the state
 
   let state;
+  let listeners = [];
 
   const getState = () => state;
 
-  return {
-    getState
+  const subscribe = listener => {
+    listeners.push(listener);
+    return () => {
+      listeners.filter(l => l !== listener);
+    };
   };
+
+  const dispatch = action => {
+    state = reducer(state, action);
+    listeners.forEach(listerner => listener());
+  };
+
+  return {
+    getState,
+    subscribe,
+    dispatch
+  };
+}
+
+//App code
+function todos(state = [], action) {
+  if (action.type === "ADD_TODO") {
+    return state.concat([action.todo]);
+  }
+
+  return state;
 }
