b84a01ed80a834e711cf6e4b9877699aaa5b511d
Taynan Siqueira <taynacid123@gmail.com>
2019-01-03 10:46:40 -0200

    add another reducer(goals) and with that the root reducer which manages both states(todos and goals)

--- index.js
+++ index.js
@@ -48,7 +48,25 @@ function todos(state = [], action) {
   }
 }
 
-const store = createStore(todos);
+function goals(state = [], action) {
+  switch (action.type) {
+    case "ADD_GOAL":
+      return state.concat([action.goal]);
+    case "REMOVE_GOAL":
+      return state.filter(goal => goal.id !== action.id);
+    default:
+      return state;
+  }
+}
+
+function app(state = {}, action) {
+  return {
+    todos: todos(state.todos, action),
+    goals: goals(state.goals, action)
+  };
+}
+
+const store = createStore(app);
 
 store.subscribe(() => {
   console.log("the new state is: ", store.getState());
